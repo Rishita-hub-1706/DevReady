883872d77601ff3ace22a8d72f1047efec5820ac
rishita <rishita1706@gmail.com>
2026-03-04 18:39:50 +0530

    Added login validation and dashboard routes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask import request, redirect, url_for
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///devready.db"
@@ -13,25 +14,36 @@ class User(db.Model):
     password = db.Column(db.String(100), nullable=False)
     role = db.Column(db.String(20), nullable=False)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        employee_id = request.form.get("employee_id")
+        password = request.form["password"]
+
+        user = User.query.filter_by(
+            employee_id=employee_id,
+            password=password
+        ).first()
+
+        if user:
+            if user.role == "hr":
+                return redirect(url_for("hr_dashboard"))
+            else:
+                return redirect(url_for("employee_dashboard"))
+        else:
+            return "Invalid credentials"
+
     return render_template("login.html")
 
-@app.route("/employee")
-def employee_dashboard():
-    return render_template("employee_dashboard.html")
-
-@app.route("/hr")
+@app.route("/hr_dashboard")
 def hr_dashboard():
-    return render_template("hr_dashboard.html")
+    return "Welcome HR Dashboard"
 
-with app.app_context():
-    if not User.query.filter_by(employee_id="HR001").first():
-        hr = User(employee_id="HR001", password="hr123", role="hr")
-        emp = User(employee_id="EMP001", password="emp123", role="employee")
-        db.session.add(hr)
-        db.session.add(emp)
-        db.session.commit()
+@app.route("/employee_dashboard")
+def employee_dashboard():
+    return "Welcome Employee Dashboard"
+
+
 
 
 if __name__ == "__main__":
