c79b74c4f2d7859c55233127c00f779ff8a99616
rishita <rishita1706@gmail.com>
2026-03-04 19:12:18 +0530

    Added flask session and secret key

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask import request, redirect, url_for
+from flask import request, redirect, url_for, sesssion
 
 app = Flask(__name__)
+app.secret_key = "dev_ready_secret_key"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///devready.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
